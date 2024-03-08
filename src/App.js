b118080a449a1be4c623f3c2227e89261d8e4d67
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-03-08 21:39:07 +0500

    This site for Our Agency - Fenix Company

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState, Component } from 'react';
+import useScrollPosition from "use-scroll-position";
+import './css/plugins.css';
+import './css/style.css';
+import './css/templete.css';
+import './css/skin/skin-3.css';
+import './plugins/slick/slick.min.css';
+import './plugins/slick/slick-theme.min.css';
+import 'react-modal-video/css/modal-video.min.css';
+
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import ThemeButton from './markup/Element/ThemeButton/ThemeButton';
+
+
+import Index7 from './markup/Pages/Index7.js';
+import Aboutus2 from './markup/Pages/Aboutus/Aboutus2';
+
+import Error404 from './markup/Pages/Error/Error404';
+
+import Expertise from './markup/Pages/Expertise';
+import Team from './markup/Pages/Team';
+import Faqs from './markup/Pages/Faqs';
+import Contact from './markup/Pages/Contact';
+import Service from './markup/Pages/Service/Service.js';
+
+import ScrollToTop from './markup/Element/ScrollToTop';
+import PortfolioFullWidth from './markup/Pages/Portfolio/PortfolioFullWidth';
+
+function App() {
+	const [body_, setbody_] = useState();
+	const [header, setHeader] = useState("fixed");
+	const [header_, setHeader_] = useState();
+	let scrollPosition = useScrollPosition();
+
+	useEffect(() => {
+		setbody_(document.querySelector("body"));
+		setHeader_(document.getElementsByClassName("main-bar-wraper"));
+	}, []);
+
+
+	var element = document.getElementById("fix-header");
+
+	if (typeof (element) != 'undefined' && element != null) {
+		header === "fixed" && scrollPosition > 10
+			? header_ && header_[0].classList.add("is-fixed")
+			: header_ && header_[0].classList.remove("is-fixed");
+	}
+	return (
+		<>
+			<div className="page-wraper">
+				<Routes>
+					<Route index path='/' element={<Index7 />} />
+					<Route path='/about-2' element={<Aboutus2 />} />
+					<Route path='/expertise' element={<Expertise />} />
+					<Route path='/team' element={<Team />} />
+					<Route path='/faqs' element={<Faqs />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/service' element={<Service />} />
+					<Route path='/portfolio-full-width' element={<PortfolioFullWidth />} />
+					<Route path='*' element={<Error404 />} />
+				</Routes>
+			</div>
+			<ScrollToTop />
+			<ThemeButton />
+		</>
+	);
+}
+
+export default App;
---
+++ src/markup/Markup.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import ThemeButton from './Element/ThemeButton/ThemeButton';
+
+
+import Index7 from './Pages/Index7';
+import Aboutus2 from './Pages/Aboutus/Aboutus2';
+
+import Error404 from './Pages/Error/Error404';
+
+import Expertise from './Pages/Expertise';
+import Team from './Pages/Team';
+import Faqs from './Pages/Faqs';
+import Contact from './Pages/Contact';
+import Service from './Pages/Service/Service.js';
+
+import ScrollToTop from './Element/ScrollToTop';
+import PortfolioFullWidth from './Pages/Portfolio/PortfolioFullWidth';
+
+class Markup extends Component {
+    render() {
+        return (<>
+            <div className="page-wraper">
+                <Routes>
+                    <Route index path='/' exact component={Index7} />
+                    <Route path='/about-2' exact component={Aboutus2} />
+                    <Route path='/expertise' exact component={Expertise} />
+                    <Route path='/team' exact component={Team} />
+                    <Route path='/faqs' exact component={Faqs} />
+                    <Route path='/contact' exact component={Contact} />
+                    <Route path='/service' exact component={Service} />
+                    <Route path='/portfolio-full-width' exact component={PortfolioFullWidth} />
+                    <Route path='*' exact component={Error404} />
+                </Routes>
+            </div>
+            <ScrollToTop />
+            <ThemeButton />
+        </>
+        )
+    }
+
+}
+
+export default Markup;
